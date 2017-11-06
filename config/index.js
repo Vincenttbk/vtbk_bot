66b96409cdd007e180141569341fab492af41032
Vincent Tan <tan_boon_kheng@ida.gov.sg>
2017-11-06 10:36:39 +0800

    first commit

---
+++ config/index.js
@@ -0,0 +1,5 @@
+const Config = {
+	telegram: 'INPUT TELEGRAM KEY HERE', //345763778:AAFbelBVTRKvwdZ2w76toGh5vC5a_4Abaj4
+	spreadsheetKey: 'INPUT GOOGLE SPREADSHEET KEY HERE' //1jHqgE852pXI6BQEgr2EiYBiwtBC4_22SI6kkg-MQyGE
+};
+module.exports = Config;
